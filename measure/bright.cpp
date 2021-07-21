7cf9109999a7de83cde45b85e49807b943b2b552
Saikoro1028 <85277984+Saikoro1028@users.noreply.github.com>
2021-07-21 14:41:19 +0900

    Bright.cpp仮完了

---
+++ measure/bright.cpp
@@ -0,0 +1,21 @@
+#include "bright.h"
+
+Bright::Main_Bright()
+{
+
+}
+
+float Bright::Bvalue()
+{
+
+}
+
+void Bright::Bload()
+{
+
+}
+
+float Bright::get_Bvalue()
+{
+
+}
---
+++ measure/bright.h
@@ -0,0 +1,9 @@
+#ifndef EV3_UNIT_BRIGHT_H_
+#define EV3_UNIT_BRIGHT_H_
+
+class Bright{
+    public:
+
+    
+    private:
+}
