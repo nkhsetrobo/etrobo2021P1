fd3f93ae4ed28feb49e82f1a00fdaed2b09a2d2a
Saikoro1028 <85277984+Saikoro1028@users.noreply.github.com>
2021-07-26 13:25:30 +0900

    アーム

---
+++ measure/Arm.h
@@ -0,0 +1,16 @@
+#ifndef EV3_UNIT_ARM_H_
+#define EV3_UNIT_ARM_H_
+
+class Arm{
+    public:
+        Arm();
+        void load(float mload);
+        float get_value();
+        void .reset();
+
+    private:
+
+        float avalue;
+};
+
+#endif
