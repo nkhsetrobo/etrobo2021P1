10891199a6dd540906afedd3c3112166bb3a2459
sirakin <haibokusya1421@gmail.com>
2021-07-26 14:50:30 +0900

    カラーmeasure途中まで

--- measure/ColorMeasure.cpp
+++ measure/ColorMeasure.cpp
@@ -1,17 +1,16 @@
 #include "ColorMeasure.h"
 
-ColorMeasure::ColorMeasure()
+ColorMeasure::ColorMeasure(const ev3api::ColorSensor& colorSensor,Bright* bright)
+    :  mColorSensor(colorSensor),mbright(bright)
 {
 }
 
 
-void ColorMeasure::get_rgb(float cvalue)
+void ColorMeasure::get_rgb()
 {
-    cvalue;
+    rgb_raw_t rgbr;
+    mColorSensor.getRawColor(&rgbr);
+    float cvalue=rgbr.r*0.21+rgbr.g*0.72+rgbr.b*0.07;
+    mbright->load(cvalue);
 }
 
-
-void ColorMeasure::load(float mload)
-{
-    =mload
-}
--- measure/ColorMeasure.h
+++ measure/ColorMeasure.h
@@ -1,13 +1,15 @@
 #ifndef EV3_UNIT_BRIGHT_H_
 #define EV3_UNIT_BRIGHT_H_
+#include "ColorSensor.h"
+#include "Bright.h"
 
 class ColorMeasure{
     public:
-        ColorMeasure();
+        explicit ColorMeasure(const ev3api::ColorSensor& colorSensor,Bright* mbright);
         void get_rgb();
-        void load();
 
     private:
-    
+        const ev3api::ColorSensor& mColorSensor;
+        Bright* mbright;
 };
 #endif
