15bf60a9f0d29b5be9a498a965c60ca810be8129
Saikoro1028 <85277984+Saikoro1028@users.noreply.github.com>
2021-07-26 14:05:35 +0900

    x座標までやりました

---
+++ measure/Xpointer.cpp
@@ -0,0 +1,21 @@
+#include "Xpointer.h"
+
+Xpointer::Xpointer()
+{
+
+}
+
+void Xpointer::load(float mload)
+{
+    xvalue=mload;
+}
+
+float Xpointer::get_value()
+{
+    return xvalue;
+}
+
+void Xpointer::reset()
+{
+    *.reset();
+}
