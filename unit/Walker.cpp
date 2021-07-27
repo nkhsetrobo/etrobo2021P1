a7fce3c1837d0a2ee7f96d272255d3731b134cb9
white1185 <stella_white@ezweb.ne.jp>
2021-07-27 17:40:38 +0900

    空のWalker作成

---
+++ unit/Walker.cpp
@@ -0,0 +1,15 @@
+#include "Walker.h"
+
+Walker::Walker()
+{
+
+}
+
+void Walker::run()
+{
+    
+}
+void Walker::init()
+{
+    
+}
---
+++ unit/Walker.h
@@ -0,0 +1,12 @@
+#ifndef EV3_UNIT_WALKER_H_    /*走法*/
+#define EV3_UNIT_WALKER_H_
+
+class Walker{
+    public:
+        Walker();
+        virtual float  run();
+        void init();
+
+};
+
+#endif
