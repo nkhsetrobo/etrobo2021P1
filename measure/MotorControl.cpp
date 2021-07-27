3d9287e51bcee9901de8ce8debceb80c7514238a
sirakin <haibokusya1421@gmail.com>
2021-07-27 09:50:23 +0900

    motor制御

---
+++ measure/MotorControl.cpp
@@ -0,0 +1,21 @@
+#include "MotorControl.h"
+
+MotorControl::MotorControl(const ){
+
+}
+
+void runcontrol(){
+
+}
+
+void armcontrol(){
+
+}
+
+void tailcontrol(){
+
+}
+
+void virtualposition(){
+
+}
