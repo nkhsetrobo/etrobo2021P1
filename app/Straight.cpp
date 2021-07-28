eef3fc25f2fe3e8ca84f9c74309a9a9091006cba
Saikoro1028 <85277984+Saikoro1028@users.noreply.github.com>
2021-07-28 16:21:11 +0900

    Straight kari

---
+++ app/Straight.cpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *  LineTracer.cpp (for LEGO Mindstorms EV3)
+ *  Created on: 2015/01/26
+ *  Implementation of the Class LineTracer
+ *  Author: Kazuhiro Kawachi
+ *  Copyright (c) 2015 Embedded Technology Software Design Robot Contest
+ *****************************************************************************/
+
+#include "LineTracer.h"
+
+/**
+ * コンストラクタ
+ * @param lineMonitor     ライン判定
+ * @param walker 走行
+ */
+LineTracer::LineTracer(const LineMonitor* lineMonitor,
+                       Walker* walker)
+    : mLineMonitor(lineMonitor),
+      mWalker(walker),
+      mIsInitialized(false) {
+}
+
+/**
+ * ライントレースする
+ */
+void LineTracer::run() {
+    if (mIsInitialized == false) {
+        mWalker->init();
+        mIsInitialized = true;
+    }
+
+    bool isOnLine = mLineMonitor->isOnLine();
+
+    // 走行体の向きを計算する
+    int direction = calcDirection(isOnLine);
+
+    mWalker->setCommand(Walker::LOW, direction);
+
+    // 走行を行う
+    mWalker->run();
+}
