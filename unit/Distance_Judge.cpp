d57e7d7260b73dab1dbf4ee3c246a4866c449b2f
Saikoro1028 <85277984+Saikoro1028@users.noreply.github.com>
2021-07-29 11:29:37 +0900

    判定：距離条件

---
+++ unit/Distance_Judge.cpp
@@ -0,0 +1,17 @@
+#include "Distance_Judge.h"
+
+Distance_Judge::Distance_Judge()
+    : Main_Judge()
+{
+
+}
+
+float Distance_Judge::distance()
+{
+    return 0;
+}
+
+bool Distance_Judge::judge()
+{
+    return false;
+}
