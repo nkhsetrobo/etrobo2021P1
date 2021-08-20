653fdad985bc9beb808f48a05e48150a62b15364
white1185 <stella_white@ezweb.ne.jp>
2021-08-20 18:22:39 +0900

    SMspeed

---
+++ section/SMspeed.h
@@ -0,0 +1,14 @@
+#ifndef EV3_UNIT_SMSPEED_H_
+#define EV3_UNIT_SMSPEED_H_
+
+#include "Section_management.h"
+
+class SMspeed : public Section_management{
+    public:
+    
+        SMspeed();
+
+    private:
+};
+
+#endif
