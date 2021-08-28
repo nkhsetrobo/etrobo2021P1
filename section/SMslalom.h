a0ceab519f976bf4f6b7119a108fc665e810c5a4
Aki-1234 <akimotoyota@gmail.com>
2021-08-28 09:31:02 +0900

    パラメータ調整

--- section/SMslalom.h
+++ section/SMslalom.h
@@ -21,7 +21,7 @@ class SMslalom : public Section_management{
             {LINETRACER,{50,1,5,8,LEFT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
             {TAIL,{1,300},TAILJUDGE,{300},-1,{},UPDATE_ALL},  
             {LINETRACER,{20,1,5,8,LEFT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
-            {ARM,{0,-50},ARMJUDGE,{-30},-1,{},UPDATE_ALL},  
+            {ARM,{0,-30},ARMJUDGE,{-30},-1,{},UPDATE_ALL},  
             {TAIL,{1,-1000},TAILJUDGE,{-900},-1,{},UPDATE_ALL}, 
             {LINETRACER,{50,1,5,50,LEFT,0}, DISTANCE,{50},-1,{}, UPDATE_ALL},
             {-1,{}, -1,{},-1,{}, UNUPDATE}
