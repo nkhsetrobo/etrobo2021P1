ad0ab0a1d5f6cfaf7e2b41074b7cb4198e60ea27
sirakin <haibokusya1421@gmail.com>
2021-09-01 18:13:41 +0900

    ss

--- section/SMslalom.h
+++ section/SMslalom.h
@@ -16,24 +16,31 @@ class SMslalom : public Section_management{
     private:
         Sonar*      mSonar;
         SonarMeasure*  mSonarMeasure;
+        float kp=10.0;
+        float ki=1;
+        float kd=0.5;
         Main_Section slalompra[100]={
+            /*スラロムに登る*/
+            {LINETRACER,{30,1,1,5,0,0}, DISTANCE,{10}, -1,{0}, UPDATE_ALL},
+            {WALKER,{0,0}, TIMEJUDGE,{30}, UNDEFINED},
+            {ARM,{1,0},ARMJUDGE,{-65},-1,{},UPDATE_ALL},
+            {WALKER,{5,0}, DISTANCE,{20}, UNDEFINED},
+            {LINETRACER,{55,1,0.5,15,0,0}, DISTANCE,{15}, -1,{0}, UPDATE_ALL},
             {WALKER,{-3,0},DISTANCE,{-3},-1,{},UPDATE_ALL},
             {ARM,{1,0},ARMJUDGE,{-10},-1,{},UPDATE_ALL}, 
             {WALKER,{10,0},DISTANCE,{17},-1,{},UPDATE_ALL},    
             {TAIL,{1,650},TAILJUDGE,{600},-1,{},UPDATE_ALL},
             {WALKER,{0,0},TIMEJUDGE,{10},-1,{},UPDATE_ALL},  
-            {WALKER,{6,0},DISTANCE,{9},-1,{}, UPDATE_ALL},
+            {WALKER,{5,0},DISTANCE,{9},-1,{}, UPDATE_ALL},
             {TAIL,{0,0},TAILJUDGE,{10},-1,{},UPDATE_ALL}, 
             {ARM,{0,-50},ARMJUDGE,{-30},-1,{},UPDATE_ALL}, 
-            {LINETRACER,{45,1,5,3,LEFT,0}, DISTANCE,{12},-1,{}, UPDATE_ALL},
-            {STRAIGHT,{30,1,5,10,-100}, DISTANCE,{2},-1,{}, UPDATE_DIST},
-            {WALKER,{0,0}, TIMEJUDGE,{20}, UNUPDATE},
-            {CURVE,{45,1,5,10,0,7}, TURN,{140},-1,{}, UNUPDATE},
-            {CURVE,{45,1,5,4,0,-10}, TURN,{80},-1,{}, UNUPDATE},
-            {WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
-            {CURVE,{45,1,5,10,0,-14}, TURN,{70},-1,{}, UNUPDATE},
-            {WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
-            {CURVE,{30,1,5,10,0,-5}, TURN,{30},-1,{}, UNUPDATE},
+            {LINETRACER,{10,1,0.5,6,LEFT,0}, DISTANCE,{6.1},-1,{}, UPDATE_ALL},
+            /*スラロム登った*/
+            {STRAIGHT,{10,1,0.5,3,-50}, DISTANCE,{1},-1,{}, UPDATE_DIST},
+            /*{WALKER,{0,0}, TIMEJUDGE,{50}, UNUPDATE},*/
+            {CURVE,{10,2,0.6,3,0,19.7}, TURN,{42},-1,{}, UNUPDATE},
+            {CURVE,{10,2,0.6,3,0,-18.2}, TURN,{40},-1,{}, UNUPDATE},
+            {CURVE,{10,2,0.6,3,0,19.4}, TURN,{30},-1,{}, UNUPDATE},
             //{WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
             /*{CURVE,{30,1,5,10,0,-10}, TURN,{35},-1,{}, UNUPDATE},
             {CURVE,{30,1,5,10,0,10}, TURN,{20},-1,{}, UNUPDATE},
