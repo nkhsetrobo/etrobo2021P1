1e4abc9352410ff623d36438116622526ba20ff6
sirakin <haibokusya1421@gmail.com>
2021-09-04 12:23:54 +0900

    A

--- section/SMslalom.h
+++ section/SMslalom.h
@@ -34,7 +34,7 @@ class SMslalom : public Section_management{
             {WALKER,{12,0},DISTANCE,{6},-1,{}, UPDATE_ALL},
             {TAIL,{0,0},TAILJUDGE,{10},-1,{},UPDATE_ALL}, 
             {ARM,{0,-50},ARMJUDGE,{-30},-1,{},UPDATE_ALL}, 
-            {LINETRACER,{14,1,0.5,6,LEFT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
+            {LINETRACER,{25,2,0.5,6,LEFT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
             /*スラロム登った*/
             {WALKER,{0,-10},TURN,{-50},-1,{}, UPDATE_ANG},
             //{STRAIGHT,{10,1,0.5,3,-50}, DISTANCE,{1},-1,{}, UPDATE_DIST},
@@ -42,17 +42,20 @@ class SMslalom : public Section_management{
             {CURVE,{10,2,0.6,11,0,22.5}, TURN,{43},-1,{}, UNUPDATE},
             {CURVE,{10,2,0.6,11,0,-19.2}, TURN,{-37},-1,{}, UNUPDATE},
             {CURVE,{10,2,0.6,11,0,22.8}, TURN,{-20},-1,{}, UNUPDATE},
-            {CURVE,{10,2,0.6,11,0,21.3}, TURN,{0},-1,{}, UNUPDATE},
+            {CURVE,{10,2,0.6,11,0,20.8}, TURN,{0},-1,{}, UNUPDATE},
             {WALKER,{11,0}, DISTANCE,{10.7},-1,{},UPDATE_DIST},
             {WALKER,{10,0},DISTANCE,{12},SONARJUDGE,{20}, UPDATE_DIST},
             //踊り場旋回
             {WALKER,{0,-10},TURN,{-85},-1,{}, UPDATE_DIST},
-            {STRAIGHT,{10,1,0.6,10,-85}, DISTANCE,{11.5},-1,{}, UPDATE_DIST},
+            {STRAIGHT,{10,1,0.6,10,-85}, DISTANCE,{13},-1,{}, UPDATE_DIST},
             //{WALKER,{0,0}, DISTANCE,{12000},-1,{}, UNDEFINED},
-            {WALKER,{0,-10}, TURN,{-135},-1,{}, UPDATE_DIST},
-            {CURVE,{10,2,0.6,6,0,18},TURN,{-60} ,-1,{}, UPDATE_DIST},
-            {STRAIGHT,{10,1,0.6,10,-85},BRIGHT,{-0.8,-1}, -1,{},UPDATE_DIST},
+            //{WALKER,{0,-10}, TURN,{-120},-1,{}, UPDATE_DIST},
+            {CURVE,{10,2,0.6,11,0,-27.5},TURN,{-120} ,-1,{}, UNUPDATE},
+            {CURVE,{10,2,0.6,6,0,25},TURN,{-85} ,-1,{}, UNUPDATE},
+            {WALKER,{10,0},DISTANCE,{15},-1,{}},
+            {CURVE,{10,1,0.6,6,0,25},BRIGHT,{-0.8,-1}, -1,{},UNUPDATE},
             {LINETRACER,{14,1,0.5,6,LEFT,0}, DISTANCE,{10},-1,{}, UPDATE_ALL},
+            {WALKER,{0,0}, DISTANCE,{12000},-1,{}, UNDEFINED},
 
             {-1,{}, -1,{},-1,{}, UNUPDATE}
                                 };
