c79abd52f46e1f211637ec1e8be6586c4736fcce
sirakin <haibokusya1421@gmail.com>
2021-09-08 00:34:43 +0900

    左スラロム完成右a完成

--- section/SMblock.h
+++ section/SMblock.h
@@ -19,7 +19,7 @@ class SMblock : public Section_management{
             {STRAIGHT,{20,1,0.8,10,-75},BRIGHT,{-0.1,-1},-1,{},UNUPDATE},
             {WALKER,{0,-10}, TURN,{-140},-1,{}, UNUPDATE},
             //{CURVE,{30,1,5,15,0,-9},TURN,{-171},-1,{},UPDATE_DIST},
-            {LINETRACER,{10,0,0.6,8,RIGHT,0}, DISTANCE,{10},-1,{}, UPDATE_DIST},
+            {LINETRACER,{10,0,0.6,8,LineTracer::LEFT_EDGE,0}, DISTANCE,{10},-1,{}, UPDATE_DIST},
             //{STRAIGHT,{30,1,1,1,-174},COLORJUDGE,{0.3,350},-1,{},UPDATE_DIST},
             {STRAIGHT,{30,1,1,30,-170},DISTANCE,{17},-1,{},UPDATE_DIST},
             //ブロック取った後
@@ -30,7 +30,7 @@ class SMblock : public Section_management{
 
             {STRAIGHT,{10,1,0.6,10,-110},BRIGHT,{-0.1,-1},-1,{},UNUPDATE},
             {WALKER,{0,-10}, TURN,{-140},-1,{}, UNUPDATE},
-            {LINETRACER,{10,0,0.6,7,RIGHT,0}, DISTANCE,{15},-1,{}, UPDATE_DIST},
+            {LINETRACER,{10,0,0.6,7,LineTracer::LEFT_EDGE,0}, DISTANCE,{15},-1,{}, UPDATE_DIST},
             //{CURVE,{30,1,5,40,0,-1},TURN,{-30},-1,{},UPDATE_ALL},
             {STRAIGHT,{30,1,1,15,185},SONARJUDGE,{4},-1,{},UPDATE_DIST},
             {WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
@@ -45,7 +45,7 @@ class SMblock : public Section_management{
             {STRAIGHT,{20,1,0.8,10,75},BRIGHT,{-0.1,-1},-1,{},UNUPDATE},
             {WALKER,{0,10}, TURN,{140},-1,{}, UNUPDATE},
             //{CURVE,{30,1,5,15,0,-9},TURN,{-171},-1,{},UPDATE_DIST},
-            {LINETRACER,{10,0,0.6,8,LEFT,0}, DISTANCE,{10},-1,{}, UPDATE_DIST},
+            {LINETRACER,{10,0,0.6,8,LineTracer::RIGHT_EDGE,0}, DISTANCE,{10},-1,{}, UPDATE_DIST},
             //{STRAIGHT,{30,1,1,1,-174},COLORJUDGE,{0.3,350},-1,{},UPDATE_DIST},
             {STRAIGHT,{30,1,1,30,170},DISTANCE,{17},-1,{},UPDATE_DIST},
             //ブロック取った後
@@ -57,7 +57,7 @@ class SMblock : public Section_management{
             {STRAIGHT,{10,0,0.6,10,110},BRIGHT,{-0.1,-1},-1,{},UNUPDATE},
             {WALKER,{0,10}, TURN,{140},-1,{}, UNUPDATE},
             //{WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
-            {LINETRACER,{10,0,0.6,7,LEFT,0}, DISTANCE,{15},-1,{}, UPDATE_DIST},
+            {LINETRACER,{10,0,0.6,7,LineTracer::RIGHT_EDGE,0}, DISTANCE,{15},-1,{}, UPDATE_DIST},
             //{CURVE,{30,1,5,40,0,-1},TURN,{-30},-1,{},UPDATE_ALL},
             {STRAIGHT,{30,1,1,15,-185},SONARJUDGE,{4},-1,{},UPDATE_DIST},
             {WALKER,{0,0}, DISTANCE,{12000}, UNDEFINED},
--- section/SMslalomb.h
+++ section/SMslalomb.h
@@ -22,7 +22,7 @@ class SMslalomb : public Section_management{
              {WALKER,{10,0},DISTANCE,{15},-1,{},UPDATE_DIST},
              {CURVE,{10,1,0.6,6,0,-20},BRIGHT,{-0.3,-1}, -1,{},UNUPDATE},
              {LINETRACER,{14,1,0.5,6,RIGHT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
-             {WALKER,{10,0},SONARJUDGE,{60},-1,{}, UPDATE_ALL},
+             {WALKER,{10,0},SONARJUDGE,{85},-1,{}, UPDATE_ALL},
                         
             {-1,{}, -1,{},-1,{}, UNUPDATE}
                                 };
@@ -30,17 +30,16 @@ class SMslalomb : public Section_management{
 
 
 
-        Main_Section rslalombpra[100]={//未変更
-             {CURVE,{10,2,0.6,11,0,24.5}, TURN,{43},-1,{}, UNUPDATE},
+        Main_Section rslalombpra[100]={
+            {CURVE,{10,2,0.6,11,0,21}, TURN,{43},-1,{}, UNUPDATE},
              //{WALKER,{0,-10},TURN,{-50},-1,{},UNUPDATE},
              //{WALKER,{10,0},DISTANCE,{10},-1,{},UNUPDATE},
             //{CURVE,{10,2,0.6,11,0,10},TURN,{-30} ,-1,{}, UNUPDATE},
-             {CURVE,{10,2,0.6,11,0,45},TURN,{85} ,-1,{}, UNUPDATE},
+             {CURVE,{10,2,0.6,11,0,45},TURN,{95} ,-1,{}, UNUPDATE},
              {WALKER,{10,0},DISTANCE,{15},-1,{},UPDATE_DIST},
              {CURVE,{10,1,0.6,6,0,20},BRIGHT,{-0.3,-1}, -1,{},UNUPDATE},
-             {LINETRACER,{14,1,0.5,6,LEFT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
-             {WALKER,{10,0},SONARJUDGE,{60},-1,{}, UPDATE_ALL},
-                        
+             {LINETRACER,{14,1,0.5,6,RIGHT,0}, DISTANCE,{8},-1,{}, UPDATE_ALL},
+             {WALKER,{10,0},SONARJUDGE,{85},-1,{}, UPDATE_ALL},
             {-1,{}, -1,{},-1,{}, UNUPDATE}
                                 };
 };
